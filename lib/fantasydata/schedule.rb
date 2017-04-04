66c815bca7e6b033829ad83cdaf4f3262bb10e59
Kyle Dayton <kyle@kohactive.com>
2017-04-04 17:45:54 -0500

    Include date_time in schedules

--- lib/fantasydata/schedule.rb
+++ lib/fantasydata/schedule.rb
@@ -4,7 +4,7 @@ module Fantasydata
   class Schedule < Fantasydata::Base
     attr_reader :game_key, :season_type, :season, :week, :date, 
                 :away_team, :home_team, :channel, :point_spread, 
-                :over_under, :stadium_id, :game_id, :global_game_id
+                :over_under, :stadium_id, :game_id, :global_game_id, :date_time
 
     def stadium
       @stadium ||= Fantasydata::Stadium.new(@attrs[:stadium_details])
